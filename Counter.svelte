202c5901c1950f75b7acd5afaaee5da2cd45e96b
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-03-07 00:28:09 +0100

    wip

---
+++ Counter.svelte
@@ -0,0 +1,23 @@
+<script>
+    let count = $state(0);
+  
+    function increment() {
+      count++;
+    }
+  
+    function decrement() {
+      count--;
+    }
+  </script>
+  
+  <div class="counter">
+    <button onclick={decrement}>-</button>
+    <span class="count">{count}</span>
+    <button onclick={increment}>+</button>
+  </div>
+  
+  <style>
+    .counter {
+      color: red;
+    } 
+  </style>
