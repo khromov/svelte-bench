9afa6f6e706db2c932cc5156fae3672d8761f85a
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2026-09-15 22:50:42 +0200

    Update run-ollama.sh

--- run-ollama.sh
+++ run-ollama.sh
@@ -20,6 +20,15 @@ MODELS=(
   "wcamaralopes/bonsai-27b-Q4_K_M-fix:latest"
   "qwen3.6:35b-a3b-q4_K_M"
   "qwen3.8:27b-mtp-q4_K_M"
+  # NEW RUN: Delete above models first
+  #"glm-4.7-flash:q4_K_M"
+  #"qwen3.6:27b-coding-mtp-q4_K_M"
+  #"gemma4:12b-it-q4_K_M"
+  #"lfm2:24b"
+  #"hf.co/bartowski/nex-agi_Nex-N2.5-mini-GGUF:Q4_K_M"
+  #"hf.co/IFM/K2-Horizon-32B-GGUF:Q4_K_M"
+  #"hf.co/IFM/K2-Horizon-MoVA-36B-A4B-GGUF:Q4_K_M"
+  #"JetBrains/mellum2-instruct-q4_k_m"
 )
 
 mkdir -p logs
